042ab40e8bb35e497436b031a86cca51d863df06
Anton <anton.ohlin@hotmail.com>
2020-12-08 20:55:15 +0100

    onClickListeners, left == right...

--- frontend/src/utils/Input.js
+++ frontend/src/utils/Input.js
@@ -2,21 +2,30 @@ export default class Input {
     constructor(){
         this.leftClickHold = false;
         this.rightClickHold = false;
+        console.log("Input constructor")
+        document.addEventListener('mousedown', this.onLeftHold);
+        document.addEventListener('mouseup', this.onLeftRelease);        
+        document.addEventListener('rightdown', this.onRightHold);
+        document.addEventListener('rightup', this.onRightRelease);
     }
     onLeftHold() {
         this.leftClickHold = true;
+        console.log("Left hold, " + this.leftClickHold)
     }
     
     onLeftRelease() {
         this.leftClickHold = false;
+        console.log("Left release, " + this.leftClickHold)
     }
 
     onRightHold() {
         this.rightClickHold = true;
+        console.log("Right hold")
     }
     
     onRightRelease() {
         this.rightClickHold = false;
+        console.log("Right release")
     }
     
     onLeftClickMove() {
