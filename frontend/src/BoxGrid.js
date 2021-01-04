b63e5e3f9eeb484042588ef0107fb0db0b37ba50
Daniel <Daniel.falklimberg@gmail.com>
2021-01-04 22:05:55 +0100

    hints fix

--- frontend/src/BoxGrid.js
+++ frontend/src/BoxGrid.js
@@ -6,7 +6,6 @@ export default class BoxGrid extends PIXI.Container {
         this.textureSheet = data.textureSheet;
         this.resultMask = new PIXI.Sprite(data.mask);
         this.boxGrid = new PIXI.Container();
-        this.hints = new PIXI.Container();
         this.addChild(this.boxGrid);
     }
 
@@ -14,7 +13,9 @@ export default class BoxGrid extends PIXI.Container {
         this.boxMap = [];
         this.hintMap = [];
         this.boxGrid.removeChildren();
-        this.hints.removeChildren();
+
+        this.removeChild(this.resultMask);
+        this.boxGrid.mask = null;
     }
 
     buildGridRandom(size) {
@@ -24,7 +25,7 @@ export default class BoxGrid extends PIXI.Container {
             for(let x = 0; x < size; x++) {
                 let box = new Box({textureSheet:this.textureSheet});
                 box.filled = Math.round(Math.random()+0.1);
-                box.color = box.filled?Math.random()*0xFFFFFF:0xFFFFFF;
+                box.color = box.filled?Math.random()*0x000000:0xFFFFFF;
                 box.position.set(x*box.width, y*box.height);
                 this.boxGrid.addChild(box);
                 this.boxMap[y][x] = box;
@@ -51,57 +52,6 @@ export default class BoxGrid extends PIXI.Container {
         this.resultMask.height = this.boxGrid.height;
     }
 
-    buildHints() {
-        let filledData = {};
-        let hintData = {};
-
-        for(let row = 0; row < this.boxMap.length; row++) {
-            for(let col = 0; col < this.boxMap[row].length; col++) {
-                filledData["row_"+row.toString()] = filledData["row_"+row.toString()]?[...filledData["row_"+row.toString()], this.boxMap[row][col].filled]:[this.boxMap[row][col].filled];
-                filledData["col_"+col.toString()] = filledData["col_"+col.toString()]?[...filledData["col_"+col.toString()], this.boxMap[row][col].filled]:[this.boxMap[row][col].filled];
-            }
-        }
-
-        Object.keys(filledData).forEach(x=>{
-            let group = 0;
-            filledData[x].forEach((y,i)=>{
-                if(y==true) group++;
-                if((y==false || i==filledData[x].length-1) && group > 0) {
-                    hintData[x] = hintData[x]?[...hintData[x], group]:[group];
-                    group = 0;
-                }
-            });
-            if(!hintData[x]) hintData[x] = [group];
-        });
-
-        console.log(filledData);
-
-        Object.keys(hintData).forEach((x)=>{
-            let text = "";
-            hintData[x].forEach((y)=>{
-                if(x.match("row")) {
-                    text = text.concat(y+" ");
-                }
-                if(x.match("col")) {
-                    text = text.concat(y+"\n");
-                }
-            });
-            text = text.substring(0, text.length-1);
-            let hintText = new PIXI.Text(text, {
-                fontFamily:"myFont",
-                fontSize:4, fill:0xFFFFFF,
-                stroke:0x000000,
-                strokeThickness:1,
-                align:"right"
-            });
-            if(x.match("row")) hintText.position.set(hintText.width*-1, this.boxMap[0][0].height * x.substring(4) + (this.boxMap[0][0].height-hintText.height)/2);
-            if(x.match("col")) hintText.position.set(this.boxMap[0][0].width * x.substring(4) + (this.boxMap[0][0].width-hintText.width)/2,hintText.height*-1);
-            console.log(hintText.y);
-            this.hints.addChild(hintText);
-        });
-        this.addChild(this.hints);
-    }
-
     getBoxMap() {
         return this.boxMap;
     }
@@ -121,7 +71,6 @@ export default class BoxGrid extends PIXI.Container {
     }
 
     revealResult() {
-        this.hints.removeChildren();
         for(let box of this.boxGrid.children) box.revealColor();
         this.addChild(this.resultMask);
         this.boxGrid.mask = this.resultMask;
--- frontend/src/Screens/GameScreen.js
+++ frontend/src/Screens/GameScreen.js
@@ -2,6 +2,7 @@ import ScreenManager from "../utils/ScreenManager.js";
 import Button from "../Button.js";
 import BackgroundManager from "../utils/BackgroundManager.js";
 import BoxGrid from "../BoxGrid.js";
+import Hints from "../Hints.js";
 
 export default class GameScreen extends PIXI.Container {
     constructor(data) {
@@ -14,8 +15,12 @@ export default class GameScreen extends PIXI.Container {
         this.boxGrid = new BoxGrid({textureSheet:this.textureSheet, mask:data.mask});
         this.addChild(this.boxGrid);
 
+        this.hints = new Hints();
+        this.addChild(this.hints);
+
         this.buttonMenu = new Button(this.textureSheet, "MENU", () => {
             document.removeEventListener("pointerup", this.isSolvedBound);
+            this.hints.removeChildren();
             ScreenManager.changeScreen("MainMenuScreen");
             BackgroundManager.changeColor("blue");
         });
@@ -26,35 +31,38 @@ export default class GameScreen extends PIXI.Container {
     }
 
     randomPuzzle(size) {
+        this.buildPuzzle(()=>this.boxGrid.buildGridRandom(size));
+    }
+
+    fixedPuzzle(json) {
+        this.buildPuzzle(()=>this.boxGrid.buildGrid(json));
+    }
+
+    buildPuzzle(puzzleCallback) {
         this.boxGrid.scale.set(1,1);
-        this.boxGrid.buildGridRandom(size);
+        puzzleCallback();
         this.boxGrid.pivot.set(this.boxGrid.width/2, this.boxGrid.height/2);
-        this.boxGrid.scale.y = 288/this.boxGrid.height*0.9;
+        this.boxGrid.scale.y = 288/this.boxGrid.height*0.8;
         this.boxGrid.scale.x = this.boxGrid.scale.y;
-        this.boxGrid.position.set(512/2, 288/2);
-        this.boxGrid.buildHints();
+        this.boxGrid.position.set(512/2, 288/2+(288-this.boxGrid.height)/2-16);
+
+        this.buildHints();
 
         document.removeEventListener("pointerup", this.isSolvedBound);
         document.addEventListener("pointerup", this.isSolvedBound);
     }
 
-    fixedPuzzle(json) {
-        this.boxGrid.scale.set(1,1);
-        this.boxGrid.buildGrid(json);
-        this.boxGrid.pivot.set(this.boxGrid.width/2, this.boxGrid.height/2);
-        this.boxGrid.scale.y = 288/this.boxGrid.height*0.9;
-        this.boxGrid.scale.x = this.boxGrid.scale.y;
-        this.boxGrid.position.set(512/2, 288/2);
-        this.boxGrid.buildHints();
-
-        document.removeEventListener("pointerup", this.isSolvedBound);
-        document.addEventListener("pointerup", this.isSolvedBound);
+    buildHints() {
+        this.hints.removeChildren();
+        this.hints.buildHints(this.boxGrid.getBoxMap(), this.boxGrid.scale.y);
+        this.hints.position.set(this.boxGrid.x-this.boxGrid.width/2,this.boxGrid.y-this.boxGrid.height/2);
     }
 
     isSolved() {
         console.log("is the puzzle solved?");
         if(this.boxGrid.isSolved()) {
             this.boxGrid.revealResult();
+            this.hints.removeChildren();
             document.removeEventListener("pointerup", this.isSolvedBound);
         }
     }
