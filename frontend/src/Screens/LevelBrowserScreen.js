7eb8bdd3253091fb3968012ab1c8300a34d7cfc4
Anton <anton.ohlin@hotmail.com>
2021-01-05 22:11:40 +0100

    hstfghdfth

--- frontend/src/Screens/LevelBrowserScreen.js
+++ frontend/src/Screens/LevelBrowserScreen.js
@@ -1,7 +1,7 @@
 import Button from "../Button.js";
 import ScreenManager from "../utils/ScreenManager.js";
 import BackgroundManager from "../utils/BackgroundManager.js";
-import Game from "../Game.js";
+import BrowserWindow from "../BrowserWindow.js";
 
 
 export default class LevelBrowserScreen extends PIXI.Container{
@@ -9,11 +9,8 @@ export default class LevelBrowserScreen extends PIXI.Container{
         super()
 
         this.textureSheet = data.textureSheet;
-
-        this.contentContainer = new PIXI.Container();
-        this.contentContainer.height = 500;
-        console.log("container height: " + this.contentContainer.height)
-        this.addChild(this.contentContainer)
+        
+        this.addChild(new BrowserWindow);
 
         this.title = new PIXI.AnimatedSprite(data.title.animations["mode_bounce"]);
         this.title.animationSpeed = 0.02;
@@ -22,6 +19,8 @@ export default class LevelBrowserScreen extends PIXI.Container{
         this.title.anchor.set(0);
         this.title.position.set((512-this.title.width)/2, 32);
         this.addChild(this.title);
+        
+        
 
         this.buttonBack = new Button(this.textureSheet, "BACK", () => {
             //ScreenManager.previousScreen();
